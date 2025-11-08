b94c78e141c502bb5c47f67391038d219140e528
H_Ghurbani <poppinghazem3@gmail.com>
2025-11-08 14:12:43 +0300

    new

---
+++ roqi_theme/README.md
@@ -0,0 +1,17 @@
+# ROQI Theme
+
+Theme app for ERPNext (Frappe) that applies ROQI branding (yellow/black), favicon and logo.
+
+- Injects CSS bundles to override primary colors
+- Sets website favicon and splash image
+
+## Usage (local bench)
+
+```
+bench get-app /path/to/roqi_theme
+bench --site yoursite install-app roqi_theme
+bench build --app roqi_theme
+```
+
+
+
---
+++ roqi_theme/roqi_theme/hooks.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025, Your Name
+
+app_name = "roqi_theme"
+app_title = "ROQI Theme"
+app_publisher = "Your Name"
+app_description = "ROQI ERP branding for ERPNext"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# Include CSS bundles (built from public/scss)
+app_include_css = "roqi_theme.bundle.css"
+web_include_css = "roqi_theme-web.bundle.css"
+
+website_context = {
+	"favicon": "/assets/roqi_theme/images/roqi-favicon.svg",
+	"splash_image": "/assets/roqi_theme/images/roqi-logo.svg",
+}
+
+
+
